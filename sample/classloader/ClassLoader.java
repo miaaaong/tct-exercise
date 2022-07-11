73c67c7412994af1c27b353e2dbf88c65cb8f84a
miaaaong <39263167+miaaaong@users.noreply.github.com>
2022-07-11 22:24:44 +0900

    class loader & reflection

---
+++ sample/classloader/ClassLoader.java
@@ -0,0 +1,72 @@
+package sample.classloader;
+
+import java.io.File;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+
+public class ClassLoader {
+
+	// URLClassLoader, Reflection
+	// 참고 - https://stackoverflow.com/questions/6219829/method-to-dynamically-load-java-class-files
+	public void loadAndInvoke() {
+		File file = new File("c:\\myclasses\\");
+
+		try {
+			// Convert File to a URL
+			URL url = file.toURI().toURL(); // file:/c:/myclasses/
+			URL[] urls = new URL[] { url };
+
+			// Create a new class loader with the directory
+			URLClassLoader cl = new URLClassLoader(urls);
+
+			// Load in the class; MyClass.class should be located in
+			// the directory file:/c:/myclasses/com/mycompany
+			Class myclass = cl.loadClass("com.mycompany.MyClass");
+			
+			// constructor
+			Constructor[] constructors = myclass.getConstructors();
+			Constructor constructor = myclass.getConstructor(String.class, int.class);
+			
+			// interfaces
+			Class[] interfaces = myclass.getInterfaces();
+			
+			// super class
+			Class superclass = myclass.getSuperclass();
+			
+			// invoke method 
+			Object instance = myclass.newInstance();
+			Method method = myclass.getMethod("example", int.class);
+			method.invoke(instance, 10);
+			
+			// static method인 경우
+			Method m = myclass.getMethod("main", String[].class);
+			String[] args = new String[0];
+			m.invoke(null, args);  // invoke the method
+			
+		} catch (MalformedURLException e) {
+		} catch (ClassNotFoundException e) {
+		} catch (NoSuchMethodException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
